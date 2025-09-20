df4ba2b9a1a825db0509df6ddb6a3c5b1525e437
pacto <kuba.kowalski21331@gmail.com>
2025-09-20 14:28:01 +0200

    feat: expose mkNeovim function
    this allows to extend the default config in another flake
    for example my nixos config

--- parts/default.nix
+++ parts/default.nix
@@ -11,29 +11,40 @@
   ];
 
   perSystem = {
-    config,
     pkgs,
     system,
     ...
-  }: {
-    _module.args.pkgs = import inputs.nixpkgs {
-      inherit system;
-      overlays = lib.attrValues self.overlays;
-      config.allowUnfree = true;
-    };
-
-    packages.default =
-      (inputs.nvf.lib.neovimConfiguration {
-        # inherit system;
+  }: let
+    mkNeovim = modules:
+      inputs.nvf.lib.neovimConfiguration {
         inherit pkgs;
 
         extraSpecialArgs = {
           inherit inputs system self;
         };
 
-        modules = [
-          ../config
-        ];
-      }).neovim;
+        modules =
+          [
+            ../config
+          ]
+          ++ modules;
+      };
+  in {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      overlays = lib.attrValues self.overlays;
+      config.allowUnfree = true;
+    };
+
+    packages.default = (mkNeovim []).neovim;
   };
+
+  flake.lib.mkNeovim = modules: system: let
+    pkgs = import inputs.nixpkgs {inherit system;};
+  in
+    inputs.nvf.lib.neovimConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = {inherit inputs system self;};
+      modules = [../config] ++ modules;
+    };
 }
